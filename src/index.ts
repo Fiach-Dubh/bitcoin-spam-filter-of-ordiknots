f90532c128a4fcfe819f9bb53f54231c7b27a438
F_Dubh <80649724+Fiach-Dubh@users.noreply.github.com>
2025-11-17 20:43:59 -0500

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { TransactionParser } from './parser.js';
+export { FilterEngine } from './filter-engine.js';
+export { LuaFilterEngine } from './lua-engine.js';
+export { P2WSHFakeMultisigDetector } from './detectors/p2wsh-detector.js';
+export { ChainedOpReturnDetector } from './detectors/opreturn-detector.js';
+export * from './types.js';
